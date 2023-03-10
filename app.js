2342308f8753406573fd676d89c65e76f1054ae4
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-03-11 00:45:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,112 @@
+//..question 1..//
+
+
+// firstName = prompt("Enter your first Name");
+// secoundName = prompt("Enter your secound Name");
+
+// fullName = [firstName," ",secoundName];
+// document.write("Hello ! MR"+" "+fullName.join(""));
+
+
+//..question 2..//
+
+
+// mobile = (prompt("Your favorite mobile phone model"));
+// strLength = mobile.length;
+// document.write("My favourite phone is : "+ mobile + "<br />");
+// document.write("Length of string : "+strLength);
+
+
+
+// //..question 3..//
+//  var str = "pakistani";
+//  var letter = prompt("choose a letter do you want");
+//  document.write("String :" +" "+ str + "<br />");
+//  document.write("Index of '"+letter+"' :"+ " "+str.indexOf(letter));
+
+
+
+//..question 4..//
+// var str = "Hello world";
+// letter = prompt("choose a letter do you want");
+// document.write("String : " +str +"<br />");
+// document.write("Last Index off '"+letter+"' : " + str.lastIndexOf(letter));
+
+
+
+//..question 5..//
+// var str = "pakistani";
+// num = prompt("put the index number do you want");
+// document.write("string : "+ str + "<br />");
+// document.write("character at index " + num +": " + str.charAt(num));
+
+
+//..question 6..//
+// var firstName = prompt("Enter your first name");
+// var secoundName = prompt("Enter your secound name");
+// document.write("Hello Mr ! "+ firstName.concat(" ",secoundName));
+
+
+
+
+//..question 7..//
+// var city = "Hyderabad";
+// document.write("City : "+ city + "<br />");
+// for (var i = 0; i < city.length; i++){
+//     if(city.slice(i , i + 5) == "Hyder"){
+//         city = "Islam"+city.slice(5);
+//         break;
+//     }
+// }
+// document.write("After replacement : " + city);
+
+
+
+// var country = "Pakistan";
+// for (var i = 0; i < country.length; i++) {
+//     if(country.slice(i , i + 3) == "Pak"){
+//         country = "Afghan" + country.slice(3);
+//         break;
+//     }
+// }
+// console.log(country);
+
+
+
+
+//..question 8..//
+// var message = "“Ali and Sami are best friends. They play cricket and football together.”";
+// document.write(message+"<br /><br /><br />");
+// document.write("<h4>After replacement</h4>"+
+// message.replaceAll("and","&")
+
+// );
+
+
+
+
+//..question 9..//
+// var num = "472"
+// var convert = Number(num);
+// document.write("Value : " + num + "<br />");
+// document.write("Type : "+ typeof(num)+ "<br />");
+// document.write("Value : " + Number(num)+ "<br />");
+// document.write("Type : "+ typeof(convert)+ "<br />");
+
+
+
+// //..question 10..//
+// var user = prompt("Type any thing");
+// document.write("User input : " + user +"<br />");
+// document.write("Upper case : "+ user.toUpperCase());
+
+
+
+
+//..question 11..//
+// var user = prompt("Type any thing");
+// document.write("User input : " + user +"<br />");
+// document.write("Title case : " + user[0].toUpperCase()+user.slice(1));
+
+
+
